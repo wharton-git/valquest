7e261e19141f02edb10729fbdc370ec0211229fb
Xeon@Sanctuary <whartonaldrick@gmail.com>
2026-02-13 10:36:48 +0300

    Add images and XML metadata for cp1 and cp2; comment out unused imports in Result.tsx

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,5 +1,6 @@
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+// import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { RotateCcw } from "lucide-react";
+import cp1 from "./../img/cp1.jpeg";
 
 interface ResultProps {
     setShow: (value: String) => void;
@@ -8,7 +9,8 @@ interface ResultProps {
 const Result = ({ setShow }: ResultProps) => {
     return (
         <div className="relative">
-            <DotLottieReact src="/catFeeling.lottie" autoplay loop />
+            {/* <DotLottieReact src="/catFeeling.lottie" autoplay loop /> */}
+            <img src={cp1} alt="cp1" className="w-1/2 mx-auto rounded-2xl"/>
             <div className="w-full flex justify-center absolute top-3/4">
                 <p>Youuupiiii</p>
             </div>
